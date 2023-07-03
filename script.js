8058a2354a12cefa0a22451190a1998479679951
Bibek <114321426+ChiefBibek@users.noreply.github.com>
2023-07-03 22:34:04 +0545

    Update script.js
    disabling the right click to stop inspect element

--- script.js
+++ script.js
@@ -61,3 +61,25 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact 
         backSpeed: 60,
         loop: true
     });
+
+
+
+
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
